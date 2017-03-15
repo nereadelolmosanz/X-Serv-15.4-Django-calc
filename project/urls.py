f8b431b8cc572277bbe6bccfee662ae20d279418
nereadelolmosanz <nereadelolmosanz@gmail.com>
2017-03-15 14:50:26 +0100

    urls añadidas

--- project/urls.py
+++ project/urls.py
@@ -6,5 +6,21 @@ urlpatterns = patterns('',
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'calc.views.main',),
+    # ^ Nada por delante
+    # $ Nada por detras
+    # ^$ Vacio
+
+    url(r'^(?P<num1>\d{1,9})\+(?P<num2>\d{1,9})', 'calc.views.add',),
+
+    url(r'^(?P<num1>\d{1,9})\-(?P<num2>\d{1,9})', 'calc.views.sub',),
+
+    url(r'^(?P<num1>\d{1,9})\*(?P<num2>\d{1,9})', 'calc.views.mult',),
+
+    url(r'^(?P<num1>\d{1,9})\/(?P<num2>\d{1,9})', 'calc.views.div',),
+
+    #url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^.+', 'calc.views.resourceError',),
+
 )
